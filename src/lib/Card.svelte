af2491ac743bf6a326bd85dd06c0b2d49ad8c0a6
Kevin Long <kevin.uu.long@gmail.com>
2025-11-12 15:25:50 -0500

    Initialize colors and pages + card

---
+++ src/lib/Card.svelte
@@ -0,0 +1,22 @@
+<script>
+    let { children, ...rest } = $props();
+</script>
+
+<div class="card">
+    {@render children?.()}
+</div>
+
+<style>
+    .card {
+        display: flex;
+        padding: 20px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        gap: 10px;
+        align-self: stretch;
+
+        border-radius: 5px;
+        border: 1px solid var(--Tertiary-Steger-Silver, #a7a5aa);
+    }
+</style>
