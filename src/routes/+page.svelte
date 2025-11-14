c44780f9b6fdbd3f0771547393896c555e7b7d36
Athenr <ningxn27@gmail.com>
2025-11-14 14:12:35 -0500

    b1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<div class="PageHeader">
+
+</div>
+
+<style>
+ .PageHeader{
+    display: flex;
+    padding: 10px 20px;
+    align-items: center;
+    gap: 20px;
+    align-self: stretch;
+    
+ }
+</style>
---
+++ src/routes/missing/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import CourseCard from "$lib/CourseCard.svelte";
+</script>
+
+<div id="background">
+    <h1 id="h1Header">Degree Audit</h1>
+    <h2 id="h2Header">Missing Courses</h2>
+</div>
+
+<style>
+#background {
+    display: flex;
+    padding: 20px 30px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 20px;
+    flex: 1 0 0;
+    align-self: stretch;
+}
+
+#h1Header {
+    color: #000;
+    font-family: "Noto Sans";
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+}
+
+#h2Header{
+    color: #000;
+    font-family: "Noto Sans";
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+}
+
+</style>
